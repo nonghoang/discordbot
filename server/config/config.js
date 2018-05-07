5029114fec893d2f5fb241dfbc3548f34a2b1b23
hoangnong <hoang.nong.it@gmail.com>
2018-05-07 22:20:25 +0700

    Update port 3008

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
-export const SERVER_PORT = 3002;
+export const SERVER_PORT = 3008;
 export const MONGODB = 'mongodb://localhost/discordbot';
 // export const URL_CHANANEL = 'https://discordapp.com/api/v6/channels/442607708051603459/messages?limit=5';
 // export const AUTHORIZATION = 'NDA4NTU4NTc0MTE4MzcxMzQ4.DVTA5A.9Nfuh0pLhJvamX-zlSkZ8ZI3Xyc';
