8e228582af42264f0ddcc2c698d1786ad7342a06
hoangnong <hoang.nong.it@gmail.com>
2018-05-10 21:25:34 +0700

    Add online account

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,9 @@
 export const SERVER_PORT = 3008;
 export const MONGODB = 'mongodb://localhost/discordbot';
+export const BOT_TOKEN = 'NDQzMDA4MzI3OTgzODI0OTA3.DdH1yw.RwoXzHOMeHsBeifVOyeNLfxeivk';
+export const SALTROUNDS = 10;
+export const SESSION_SECRET = 'mysupersecret';
+export const SESSION_TTL = '1h';
 // export const URL_CHANANEL = 'https://discordapp.com/api/v6/channels/442607708051603459/messages?limit=5';
 // export const AUTHORIZATION = 'NDA4NTU4NTc0MTE4MzcxMzQ4.DVTA5A.9Nfuh0pLhJvamX-zlSkZ8ZI3Xyc';
 export const AUTHORIZATION = 'mfa.INzUxB7FLMP9ObX9iwqvz0dg6xjH4rKAm2YjWR09NqoLbSBYVfHZ5s-IVJi9SMM-QtCjDPFvr9A5hgVzv5E_';
@@ -7,15 +11,53 @@ export const HEADERS = {
     'accept-language': 'en-US',
     'authorization': AUTHORIZATION
 };
-export const BOT_TOKEN = 'NDQzMDA4MzI3OTgzODI0OTA3.DdH1yw.RwoXzHOMeHsBeifVOyeNLfxeivk';
+export const TOKENS = [
+    'NDQzMjI4NjA3MzMxMDQxMjkx.DdPXpQ.T95at3LWq44UcpeUIYAYIlAbqaU',
+    'NDQzOTU3MTE3NTE5NjU5MDA5.DdU7DQ.gWwchCyT7qamdhNgMnt0ouEQw7c',
+    'NDQzOTY2MzkxNjM3MjQ1OTUy.DdVDxA.wguLr-gqTH6IO1CvGG7Fl51OGoE',
+    'NDQzOTcyMzA1NjY4ODAwNTEy.DdVMrw.tNk5NDDC96-mykUwP-zHA8CZhEM',
+    'NDQzOTc4MTE1MjI0NDM2NzM2.DdVSgQ.29HhQP5d3YylrrPropQqei8Wf2w',
+    'NDQzOTgzMTQ4MzAxNDE4NTA2.DdVTIw.NoFJZxDINtNUcMstuz0sTcltWCI',
+    'NDQ0MDEwNzgyOTA5NTMwMTEz.DdVs4Q.EbFK_bE8ler3nFO3tYr0roPKubw',
+    'NDQ0MDExNjgwNjEyMDg5ODYw.DdVtyw.znkba9z3LB0XZ7DrIJp9m0qd1h0',
+    'NDQ0MDEyMjMzNzk4NzEzMzQ0.DdVucQ.N8qZrWLl2iGD1IWsCj18x_S8Dn8'
+];
 export const CHANNELS = [
 // {
 //     from: 'https://discordapp.com/api/v6/channels/442607708051603459/messages?limit=5',
 //     to: '442243825272881155'
 // },
 {
-    from: 'https://discordapp.com/api/v6/channels/426129783928258570/messages?limit=5',
-    to: '443009905218289666'
+    from: 'https://discordapp.com/api/v6/channels/418906938932461570/messages?limit=5',
+    to: '443610174993727488'
+},
+{
+    from: 'https://discordapp.com/api/v6/channels/418906970989395968/messages?limit=5',
+    to: '443610257428578316'
+},
+{
+    from: 'https://discordapp.com/api/v6/channels/422932511291605003/messages?limit=5',
+    to: '443610599470137347'
+},
+{
+    from: 'https://discordapp.com/api/v6/channels/420918984720908288/messages?limit=5',
+    to: '443610675353354250'
+},
+{
+    from: 'https://discordapp.com/api/v6/channels/423929153029406728/messages?limit=5',
+    to: '443610716843409408'
+},
+{
+    from: 'https://discordapp.com/api/v6/channels/415133171068305408/messages?limit=5',
+    to: '443610829082984448'
+},
+{
+    from: 'https://discordapp.com/api/v6/channels/428065115338964993/messages?limit=5',
+    to: '443610928878190595'
+},
+{
+    from: 'https://discordapp.com/api/v6/channels/417211893845852164/messages?limit=5',
+    to: '443611112731312134'
 },
 {
     from: 'https://discordapp.com/api/v6/channels/430914683373879297/messages?limit=5',
@@ -24,11 +66,17 @@ export const CHANNELS = [
 {
     from: 'https://discordapp.com/api/v6/channels/433299853758562305/messages?limit=5',
     to: '443010077985734667'
+},
+{
+    from: 'https://discordapp.com/api/v6/channels/422941641020801024/messages?limit=5',
+    to: '443611885515046922'
+},
+{
+    from: 'https://discordapp.com/api/v6/channels/426129783928258570/messages?limit=5',
+    to: '443009905218289666'
+},
+{
+    from: 'https://discordapp.com/api/v6/channels/415034765499039745/messages?limit=5',
+    to: '443691453756211211'
 }
-];
-export const SALTROUNDS = 10;
-export const SESSION_SECRET = 'mysupersecret';
-export const SESSION_TTL = '1h';
-export const TOKENS = [
-    'NDQzMjI4NjA3MzMxMDQxMjkx.DdPXpQ.T95at3LWq44UcpeUIYAYIlAbqaU'
 ];
