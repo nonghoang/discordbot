5a7245039c9e722d63f802db7f2289e2b0dd826b
hoangnong <hoang.nong.it@gmail.com>
2018-05-17 23:20:04 +0700

    Update config mesage

---
+++ server/config/message.js
@@ -0,0 +1,18 @@
+export const CODE = {
+    400: 'Error client site',
+    401: 'Login already used',
+    402: 'Email already used',
+    403: 'Change password false',
+    405: 'Reset password false',
+    406: 'Get user not found',
+    407: 'Update user false',
+    408: 'Create user false',
+    409: 'Authenticate user false',
+    410: 'Unauthenticated',
+    500: 'Error server site',
+    501: 'Register error when create new user',
+    502: 'Error when update user',
+    503: 'Change password false when save new password',
+    505: 'Reset password false save new password',
+    506: 'Delete user false'
+};
